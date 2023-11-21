5366dfb343e99247e27dec6468e02e000b0cc6c5
Aasimka <chabritskaya.vika@bk.ru>
2023-11-21 13:30:48 +0300

    first commit no errors

---
+++ src/production/first/Main.java
@@ -0,0 +1,11 @@
+package production.first;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MyApplication med = new MyApplication("Списки");
+        med.setVisible(true);
+        med.setResizable(false);
+        med.setLocationRelativeTo(null);
+    }
+}
---
+++ src/production/second/Main.java
@@ -0,0 +1,10 @@
+package production.second;
+
+public class Main {
+    public static void main(String[] args) {
+        MyApplication med = new MyApplication("Оформление книг");
+        med.setVisible(true);
+        med.setResizable(false);
+        med.setLocationRelativeTo(null);
+    }
+}
